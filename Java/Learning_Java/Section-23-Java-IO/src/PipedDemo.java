2777eac48fe58e989fc3b87b0f00217e7a8b39c2
Kunjesh9867 <kunjeshramani9867@gmail.com>
2024-04-07 19:51:38 -0400

    concept of random access file added

--- Java/Learning_Java/Section-23-Java-IO/src/PipedDemo.java
+++ Java/Learning_Java/Section-23-Java-IO/src/PipedDemo.java
@@ -1,2 +1,81 @@
-public class PipedDemo {
+import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class Producer extends Thread{
+    OutputStream os;
+
+    public Producer(OutputStream os){
+        this.os = os;
+    }
+
+    @Override
+    public void run(){
+        int count = 1;
+        while (true){
+            try{
+                os.write(count);
+                os.flush();
+                System.out.println("Producer: "+count);
+                System.out.flush();
+
+                Thread.sleep(10);
+
+
+                count++;
+
+
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
+    }
 }
+
+
+class Consumer extends Thread {
+    InputStream is;
+
+    public Consumer(InputStream is) {
+        this.is = is;
+    }
+
+    @Override
+    public void run() {
+        int x = 1;
+        while (true) {
+            try {
+                is.read();
+                System.out.println("Consumer: " + x);
+                System.out.flush();
+                x++;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
+
+
+
+public class PipedDemo {
+    public static void main(String[] args) throws Exception{
+
+        PipedInputStream pis = new PipedInputStream();
+        PipedOutputStream pos = new PipedOutputStream();
+
+
+        pis.connect(pos);
+        // pos.connect(pis);
+        Producer p = new Producer(pos);
+        Consumer c = new Consumer(pis);
+
+
+        p.start();
+        c.start();
+
+    }
+}
+
---
+++ Java/Learning_Java/Section-23-Java-IO/src/RandomAccessDemo.java
@@ -0,0 +1,42 @@
+import java.io.RandomAccessFile;
+import java.sql.SQLOutput;
+
+public class RandomAccessDemo {
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile rf = new RandomAccessFile("G:/My Drive/GitHub/Languages/Java/Learning_Java/Section-23-Java-IO/src/textfiles/randomAccessFile.txt", "rw");
+
+
+        /*
+        System.out.println(rf.readLine());
+        rf.write('K');
+        rf.write('U');
+        rf.write('N');
+        rf.write('J');
+        rf.write('E');
+        rf.write('S');
+        rf.write('H');
+
+         */
+
+        // If we want to see the change, then we have to open the file
+        // We cannot again use rf.readLine() because the pointer is moving
+
+
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+
+        rf.write('d');
+        System.out.println((char)rf.read());
+        rf.skipBytes(3);
+        System.out.println((char)rf.read());
+
+
+        rf.seek(3); // d => You always have to count from the beginning of the file
+        System.out.println((char)rf.read());
+        System.out.println(rf.getFilePointer());
+        rf.seek(rf.getFilePointer() + 2); // From current position
+         
+
+    }
+}
