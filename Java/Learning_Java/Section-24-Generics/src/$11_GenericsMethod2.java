9da21dee40cd5ab08a6fb18caf6194b1192d9cc3
Kunjesh9867 <kunjeshramani9867@gmail.com>
2024-04-09 13:44:57 -0400

    Generics completed

---
+++ Java/Learning_Java/Section-24-Generics/src/$11_GenericsMethod2.java
@@ -0,0 +1,42 @@
+class MyArr<T> {
+    T[] arr = (T[])new Object[5];
+
+
+    public void append1(int index, T element){
+        arr[index] = element;
+    }
+
+    public T[] arrey1(){
+        return arr;
+    }
+
+
+}
+
+
+
+
+public class $11_GenericsMethod2 {
+//    static void fun(MyArr obj){
+//        obj.arrey1();
+//    }
+
+//    static void fun(MyArr<?> obj){
+//        obj.arrey1();
+//    }
+
+    static void fun(MyArr<? extends Number> obj){ // ? super Number
+        obj.arrey1();
+    }
+    public static void main(String[] args) {
+
+        MyArr<String> ex1 = new MyArr<>();
+        ex1.append1(0,"Kunjesh");
+        ex1.append1(0,"Ramani");
+
+        MyArr<Integer> ex2 = new MyArr<>();
+        ex2.append1(0,0);
+        ex2.append1(0,1);
+
+    }
+}
